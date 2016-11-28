2401b43ad9f9ed7cb1d6605193a6e852d42f7289
Louis de Beaumont <louis.de-beaumont14@imperial.ac.uk>
2016-11-28 19:49:42 +0000

    Remove DIV tags that contain matches. Not all matches successfully removed, check other types of tag.

--- safespace/safe.js
+++ safespace/safe.js
@@ -1,20 +1,40 @@
-let body = getBodyElements();
+let body = document.getElementsByTagName('BODY')[0];
 
-for (var i = 0; i < body.length; i++) {
-  let node = body[i];
-  if (containsMatch(node.textContent) != null) {
-    // Node contains match
+if (containsMatch(body.textContent) != null) {
+  explore(body);
+}
+
+function explore(node) {
+  let children = node.childNodes;
+  let matchingChildren = [];
+
+  /* Populate matching children. We drop any non-matching children to avoid
+     exceeding call stack limit. */
+  children.forEach(function(child, index, childrenObj) {
+    if (containsMatch(child.textContent) != null && child.nodeName == 'DIV') {
+      matchingChildren.push(child);
+    }
+  });
+
+  /* If there are no children DIVs which match, delete this DIV. */
+  if (matchingChildren.length == 0) {
+    // We have to check for other types of node containing matches here.
+    node.remove();
   }
+
+  /* Explore the children nodes which match the regex and are a DIV. */
+  matchingChildren.forEach(function(child, index, childrenObj) {
+    explore(child);
+  });
 }
 
-function getBodyElements() {
-  let all = document.getElementsByTagName("*");
-  let elements = [].slice.call(all);
-  let bodyIndex = elements.map(function(e) { return e.nodeName; }).indexOf('BODY');
-  return elements.slice(bodyIndex, elements.length - 1);
-}
-
+/* Performs regex matching. */
 function containsMatch(string) {
-  let regex = /TestMatchString/g;
+  let regex = /test match/g;
   return string.match(regex);
 }
+
+// TODO: Notice updates to page content and recheck page
+// e.g. When scrolling down on Facebook and new content appears, does it get removed?
+// Prediction: no
+// Tested with "commented" 
