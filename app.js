d78d4c1d4d757fb2f10970d91f814ecfb775a0c2
Louis de Beaumont <louis.de-beaumont14@imperial.ac.uk>
2017-01-18 13:09:03 +0000

    Store new words in chrome.storage.sync in popup

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let STORAGE_KEY = 'triggering';
 let ENTER_BUTTON_ID = 13;
 let input;
 
@@ -8,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
   input.addEventListener('keypress', inputPress);
 
   /* Populate filtered words list. */
-})
+});
 
 function inputPress(event) {
   if (event.keyCode != ENTER_BUTTON_ID) {
@@ -19,4 +20,23 @@ function inputPress(event) {
   if (inputString == null || inputString == "") { return; }
 
   /* Add word to filtered words list. */
+  addWord(inputString);
+}
+
+function addWord(word) {
+  /* Get stored object. */
+  chrome.storage.sync.get(STORAGE_KEY, function (result) {
+    let storedWords = result.triggering;
+    if (storedWords == null) {
+      storedWords = [];
+    }
+
+    /* If the word is not already stored, add it and save. */
+    if (storedWords.indexOf(word) == -1) {
+      storedWords.push(word);
+      chrome.storage.sync.set({STORAGE_KEY: storedWords}, function() {
+        console.log('saved ' + storedWords);
+      });
+    }
+  });
 }
